cded6a5761ed7584ed707d9d54a9f0186430a957
jakariachynabil10 <jakariachynabil@gmail.com>
2023-10-15 01:18:42 +0600

    data is almost done

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rve683o.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,7 +26,9 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -36,16 +36,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
 app.get("/", (req, res) => {
-    res.send("Black Stars is runing");
-  });
-
-
+  res.send("Black Stars is runing");
+});
 
 app.listen(port, (req, res) => {
   console.log("Black Stars is sitting on port", port);
