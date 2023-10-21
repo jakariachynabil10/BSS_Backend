162f7bc9c4f2bbafbafd6b4ef4c9d32be962bd6f
jakariachynabil10 <jakariachynabil@gmail.com>
2023-10-21 19:44:38 +0600

    updatePlayer done

--- index.js
+++ index.js
@@ -25,25 +25,78 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const allPlayers = client.db("BlackStars").collection("AllPlayers");
 
-    const allPlayers = client.db("BlackStars").collection("AllPlayers")
+    app.get("/allPlayers", async (req, res) => {
+      const result = await allPlayers.find().toArray();
+      console.log(result);
+      res.send(result);
+    });
 
-
-
-    app.get("/allPlayers", async(req, res)=>{
-      const result = await allPlayers.find().toArray()
-      res.send(result)
-    })
-
-    app.get("/allPlayers/:id", async(req, res)=>{
-      const id = req.params.id
+    app.get("/allPlayers/:id", async (req, res) => {
+      const id = req.params.id;
       const result = await allPlayers.findOne({
-        _id : new ObjectId(id)
-      })
-      res.send(result)
-    })
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    app.post("/updatePlayer/:id", async (req, res) => {
+      const id = req.params.id; // Extract the player's _id from the URL
+      const updateData = req.body;
+      console.log(id, updateData);
+    
+      try {
+        // Update the player's details based on their _id
+        const currentPlayer = await allPlayers.findOne({ _id: new ObjectId(id) });
+
+        updateData.PL = (currentPlayer.PL || 0) + 1;
+
+        let winCounter = currentPlayer.winCounter || 0;
+        let drawCounter = currentPlayer.drawCounter || 0;
+        let loseCounter = currentPlayer.loseCounter || 0;
+    
+
+        // Determine 'PTS' based on the match result
+        if (updateData.MatchResult === "Win") {
+          winCounter += 1;
+          if (updateData.category === "Community") {
+            updateData.PTS = (currentPlayer.PTS || 0) + 15;
+          } else if (updateData.category === "Non-Community") {
+            updateData.PTS = (currentPlayer.PTS || 0) + 10;
+          } else if (updateData.category === "Intra-Club") {
+            updateData.PTS = (currentPlayer.PTS || 0) + 5;
+          }
+        } else if (updateData.MatchResult === "Draw") {
+          drawCounter += 1;
+          if (updateData.category === "Community") {
+            updateData.PTS = (currentPlayer.PTS || 0) + 8;
+          } else if (updateData.category === "Non-Community") {
+            updateData.PTS = (currentPlayer.PTS || 0) + 5;
+          } else if (updateData.category === "Intra-Club") {
+            updateData.PTS = (currentPlayer.PTS || 0) + 3;
+          }
+        } else {
+          loseCounter += 1;
+          updateData.PTS = currentPlayer.PTS || 0;
+        }
+
+        updateData.winCounter = winCounter;
+        updateData.drawCounter = drawCounter;
+        updateData.loseCounter = loseCounter;
 
 
+        const result = await allPlayers.updateOne(
+          { _id: new ObjectId(id) }, // Provide a filter to identify the document to update
+          { $set: updateData } // The update data
+        );
+    
+       res.send({result})
+      } catch (err) {
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
